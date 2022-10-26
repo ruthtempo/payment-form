dc2e8192fedfc3ab7034cb17957d2abb233b0911
Ruth Temporal <ruthtemp@gmail.com>
2022-10-26 20:43:59 +0200

    form structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Row, Col, Container } from "react-bootstrap";
+import { PaymentForm } from "./PaymentForm";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="h-100">
+      <Row>
+        <Col lg={{ span: 4, offset: 4 }}>
+          <PaymentForm />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
